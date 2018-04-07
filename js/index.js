ba6b4fe8be833f99addf8e7dcd5f4d08517c4947
4rio19 <nburressivc@gmail.com>
2018-04-06 20:35:27 -0500

    animation and background

--- js/index.js
+++ js/index.js
@@ -10,10 +10,10 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-        gravity: { y: 300 },
-        debug: false
+      gravity: { y: 0 },
+      debug: false
     }
-},
+  },
 };
 var platforms;
 var game = new Phaser.Game(config);
@@ -21,10 +21,55 @@ var game = new Phaser.Game(config);
 function preload() {
 
   this.load.atlas("hitman", "assets/Spritesheet/hitman.png", "assets/Spritesheet/hitman.json");
+  this.load.image("sky", "assets/PNG/sky.png");
 }
 
 function create() {
-  this.add.sprite(100, 100, 'hitman', 'hitman1_stand.png');
+  cursors = this.input.keyboard.createCursorKeys();
+  this.add.sprite(400, 300, 'sky');
+  player = this.physics.add.sprite(100, 450, 'hitman');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('hitman', { start: 'hitman1_hold', end: 'hitman_hold' }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'hitman', frame: 'hitman1_gun' }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('hitman', { start: 'hitman1_stand', end: 'hitman_hold' }),
+    frameRate: 10,
+    repeat: -1
+  });
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown)
+{
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+}
+else if (cursors.right.isDown)
+{
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+}
+else
+{
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+}
+}
